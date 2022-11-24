9e1bc7333e72a9c92b227968b89ec05e8d2eba5b
David Puerta Martos <dpuerta@usal.es>
2022-11-24 18:51:53 +0100

    exercise 1 done

--- index.js
+++ index.js
@@ -27,17 +27,9 @@ let authors = [
 ]
 
 /*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
  * English:
  * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
  * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conección con el libro
 */
 
 let books = [
@@ -94,11 +86,27 @@ let books = [
 
 const typeDefs = gql`
   type Query {
+    authorCount: Int!
+    bookCount: Int!
+  }
+  type Author {
+    name: String!
+    born: Int
+    id: ID!
+  }
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
   }
 `
 
 const resolvers = {
   Query: {
+    authorCount: () => authors.length,
+    bookCount: () => books.length
   }
 }
 
